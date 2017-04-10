5e478dcd9074759d019ef943d77b0e08be9152aa
annelai <sendmemailann@gmail.com>
2017-04-10 19:04:38 -0400

    add feature.py

---
+++ feature.py
@@ -0,0 +1,6 @@
+"""
+Input:
+    data_id/train.p and data_id/valid.p which are list of business idx
+Output:
+    data/train_f.p and data/valid_f.p which are pairs in format (a list of features, ans)
+"""
