432e3aa810a7d0520ce240024a663a3fe0b12934
annelai <sendmemailann@gmail.com>
2017-04-18 21:19:24 -0400

    init feature skeleton

--- feature.py
+++ feature.py
@@ -1,8 +1,11 @@
 """
 Input:
     data_id/train.p and data_id/valid.p which are list of business idx
+
 Output:
     data/train_f.p and data/valid_f.p which are pairs in format (a list of features, ans)
+    X = [name_size, name_polar, name_clarity, category, city, state, stars, review_count, popularity, age, pos_score, neg_score, elite_user]
+    y = [SI]
 """
 import pickle
 
@@ -21,35 +24,36 @@ with open("dicts/store_user.p", "r") as f:
 # with open("dicts/meta.p") as f:
     # meta = pickle.load(f)
 
+curr_time =
+
 def feature(ids):
     # Making Features
     data_f = []
+    y = []
 
     for i in range(len(ids)):
         business_id = ids[i]
         row = []
 
-    return data_f
+
+        data_f.append(row)
+        y.append(get_shutdown_index())
+
+    return data_f, y
 
 def run():
 
     # Making Features for Training
     with open("data_id/train.p", "r") as f:
         train_id = pickle.load(f)
-
-    train_f = feature(train_id)
-
     with open("data/train_f.p", "w") as f:
-        pickle.dump(train_f, f)
+        pickle.dump(feature(train_id), f)
 
     # Making Features for Validation
     with open("data_id/valid.p", "r") as f:
         valid_id = pickle.load(f)
-
-    valid_f = feature(train_id)
-
     with open("data/valid_f.p", "w") as f:
-        pickle.dump(valid_f, f)
+        pickle.dump(feature(valid_id), f)
 
 
 run()
