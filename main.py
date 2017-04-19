52109f7975a5f11a44c4c71241ae497c8ad5d9ab
annelai <sendmemailann@gmail.com>
2017-04-18 20:41:25 -0400

    add main_sklearn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from cv import *
-from sklearn import svm
+from sklearn import svm, model_selection
 
 def main():
     # Parameter settings
@@ -11,6 +11,7 @@ def main():
     test_err = 0
 
     print "Start training models with', K, '-fold cross validation..."
+
     cv(K)
     for k in range(1, K+1):
         print k, "-fold"
@@ -48,5 +49,30 @@ def main():
     print "Validation Error:", np.power(valid_predict - valid_ans,2).sum()
 
 
+def main_sklearn():
+    # Parameter settings
+    K = 5
+
+    print "Start training models with', K, '-fold cross validation..."
+
+    with open("data/train_f.p", "w") as f:
+        tr_features, tr_ans = pickle.load(f)
+
+    clf = svm.SVR()
+    scores = model_selection.cross_val_score(clf, tr_features, tr_ans, cv=K)
+
+    # Score in MSE
+    print scores
+    print "Testing Error:", np.sum(scores)/len(scores)
+
+    # Build Model (with whole data)
+    with open("data/valid_f.p", "w") as f:
+        valid_features, valid_ans = pickle.load(f)
+
+    clf.fit(tr_features, tr_ans)
+    # Validation
+    print "Training Error:", clf.score(tr_features, tr_ans)
+    print "Validation Error:", clf.score(valid_features, valid_ans)
+
 if __name__ == "__main__":
-    main()
+    main_main_sklearn()
